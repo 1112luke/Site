cf36487488a8ebe146fcfb449806eedee1b3b623
Luke Scholler <lukescholler@gmail.com>
2025-03-16 16:13:44 -0400

    spelling

--- src/Blog/Posts/Post7/Post7.jsx
+++ src/Blog/Posts/Post7/Post7.jsx
@@ -8,8 +8,8 @@ export default function Post7() {
             </p>
             <ol>
                 <li>
-                    Thinking in code provides an easy gageway for realizeing
-                    your solutions programmatically
+                    Thinking in code provides an easy gateway for realizing your
+                    solutions programmatically
                 </li>
                 <li>
                     Thinking in code provides an incredibly solid framework for
