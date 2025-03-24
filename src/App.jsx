77888f03263d01e29ffe9c56be761ea68d15055d
Luke Scholler <lukescholler@gmail.com>
2025-03-24 13:21:53 -0400

    404

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Pidemo from "./Codes/PID/Pidemo";
 //google analytics
 import ReactGA from "react-ga";
 import Lynley from "./Codes/Lynley/Lynley";
-import Nopath from "./Blog/Components/NoPath";
+import Nopath from "./Blog/Components/Nopath";
 const TRACKING_ID = "G-5B6SM3E66B"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 
